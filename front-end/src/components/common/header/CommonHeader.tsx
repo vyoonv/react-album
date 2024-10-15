4d3fe8d326429cef367d6517213af39a557e3676
vyoonv <156267745+vyoonv@users.noreply.github.com>
2024-10-15 13:25:46 +0900

    googlelogin

--- front-end/src/components/common/header/CommonHeader.tsx
+++ front-end/src/components/common/header/CommonHeader.tsx
@@ -1,9 +1,27 @@
 import { useNavigate } from 'react-router-dom'
 import styles from './CommonHeader.module.scss'
+import { useState } from 'react'
+import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google'
 
 function CommonHeader() {
 
   const navigate = useNavigate()
+
+  // 로그인 
+  const [isLoggedIn, setIsLoggedIn] = useState(false); 
+  const [user, setUser] = useState({ name: '', email: ''}); 
+  
+  // 로그인 성공 시 
+  const handleLoginSuccess = (credentialResponse) => {
+    const profile = JSON.parse(atob(credentialResponse.credential.split('.')[1])); 
+    setUser({name: profile.name, email: profile.email}); 
+    setIsLoggedIn(true); 
+  }
+
+  // 로그인 실패 시 
+  const handleLoginFailure = () => {
+    console.log('로그인 실패'); 
+  }
 
   // 페이지 이동 
   const moveToPage = (filter: string) => {
@@ -12,17 +30,28 @@ function CommonHeader() {
   }
 
   return (
-    <header className={styles.header}>
-        <div className={styles.header__logoBox} onClick={() => moveToPage('main')}>
-            <img src='src/assets/images/image-logo.png' alt='' className={styles.header__logoBox__logo}/>
-            <span className={styles.header__logoBox__title}>PhotoSplash</span>
-        </div>
-        <div className={styles.header__profileBox}>
-            <button className={styles.header__profileBox__button}>사진제출</button>
-            <button className={styles.header__profileBox__button} onClick={() => moveToPage('bookmark')}>북마크</button>
-            <span className={styles.header__profileBox__userName}>yoon | yoon@youtube.com</span>
-        </div>
-    </header>
+    <GoogleOAuthProvider clientId='716214152050-hos6gdk3ckf321qg9b86sjkpb6b5mmes.apps.googleusercontent.com'>
+      <header className={styles.header}>
+          <div className={styles.header__logoBox} onClick={() => moveToPage('main')}>
+              <img src='src/assets/images/image-logo.png' alt='' className={styles.header__logoBox__logo}/>
+              <span className={styles.header__logoBox__title}>YoonSplash</span>
+          </div>
+          <div className={styles.header__profileBox}>
+            {isLoggedIn ? (
+              <>
+              <button className={styles.header__profileBox__button}>사진 제출</button>
+              <button className={styles.header__profileBox__button} onClick={() => moveToPage('bookmark')}>북마크</button>
+              <span className={styles.header__profileBox__userName}>{user.name} | {user.email}</span>
+              </>
+            ) : (
+              <GoogleLogin 
+                  onSuccess={handleLoginSuccess}
+                  onError={handleLoginFailure}
+                />  
+            )}
+          </div>
+      </header>
+      </GoogleOAuthProvider>
   )
 }
 
